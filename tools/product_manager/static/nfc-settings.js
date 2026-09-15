9f0131ae2547b3ef1d1069e10aeeccaea44ad5e3
BGSTUDIO <berkantberkay09@gmail.com>
2026-09-15 03:44:07 +0300

    qw

--- tools/product_manager/static/nfc-settings.js
+++ tools/product_manager/static/nfc-settings.js
@@ -1,4 +1,4 @@
-/* BG Studio 3D Product Manager UI v3.1.73-R1 */
+/* BG Studio 3D Product Manager UI v3.1.74 */
 const $=id=>document.getElementById(id);
 const standardDefs=[
   ['baslangic','Başlangıç','10 masa · 30 NFC'],
